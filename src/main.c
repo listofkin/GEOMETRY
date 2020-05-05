d81184d416f096c393bdda16348db0d8d357740e
listofkin <listofkin@gmail.com>
2020-05-05 17:39:49 +0700

    Fixed files geometry.c and geometry.h and main.c

--- src/main.c
+++ src/main.c
@@ -1,26 +1,26 @@
 #include "geometry.h"
 #include <stdio.h>
 
-int main(){
+int main()
+{
     float r, S, P;
-    int x = 0, y = 0; 
+    int x = 0, y = 0;
     char name[30];
     printf("Enter a shape name \n");
-    scanf("%s",name);
-    if(figure(name) == 0){
+    scanf("%s", name);
+    if (figure(name) == 0) {
         printf("Enter coordinates: ");
-        scanf("%d %d",&x,&y);
+        scanf("%d %d", &x, &y);
         printf("Radius: ");
-        scanf("%f",&r);
+        scanf("%f", &r);
         S = Ploshad(r);
         P = Perimetr(r);
-    }
-    else{
+    } else {
         printf("Error. Incorrect data entered.\n");
-        return 0;    
+        return 0;
     }
     printf("%s(%d %d %.2f)\n", name, x, y, r);
-    printf("Ploshad = %.2f\n",S);
-    printf("Perimetr = %.2f\n",P);
+    printf("Ploshad = %.2f\n", S);
+    printf("Perimetr = %.2f\n", P);
     return 0;
 }
