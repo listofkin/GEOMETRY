11a5d0b343dadfda69a62b9ebd2328bd346a5f36
listofkin <listofkin@gmail.com>
2020-05-23 00:53:39 +0700

    Fixed Intersection.h

--- src/Intersection.h
+++ src/Intersection.h
@@ -1,7 +1,4 @@
 #ifndef INTERSECTION_H
 #define INTERSECTION_H
-
-int Intersection(
-    double x1, double y1, double x2, double y2, float r1, float r2);
-
+int Intersection(double x1, double y1, double x2, double y2, float r1, float r2);
 #endif // INTERSECTION_H
