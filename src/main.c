a2f3d9dda1bf683468d2fc5759538e119859962f
listofkin <listofkin@gmail.com>
2020-05-26 05:31:32 +0700

    Fixed main.c

--- src/main.c
+++ src/main.c
@@ -1,75 +1,80 @@
 #include "Intersection.h"
 #include "geometry.h"
-#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-using namespace std;
+
 int main()
 {
     float r1, r2;
     float S1, P1, S2, P2;
-    double x1, y1, x2, y2;
+    int x1, y1, x2, y2;
     char name[30];
     int e1, e2, e3, e4;
-    cout << "Название фигуры " << endl;
+    printf("Название фигуры \n");
     scanf("%s", name);
     if (figure(name) == 0) {
-        cout << "Координаты окружности: " << endl;
-        cin >> x1 >> y1;
-        if (x1 != (int)x1 || y1 != (int)y1) {
-            cout << "Координаты должны быть целыми! " << endl;
+        printf("Координаты окружности: \n");
+        if(scanf("%d",&x1) != 1) {
+            printf("Координаты должны быть целыми! \n");
             return 0;
         }
+	if(scanf("%d",&y1) != 1) {
+	    printf("Координаты должны быть целыми! \n");
+	    return 0;
+	}
         printf("Радиус: ");
-        cin >> r1;
+        scanf("%f",&r1);
         e1 = Ploshad(r1);
         if (e1 == 1) {
-            cout << "Неверный радиус " << endl;
+            printf("Неверный радиус \n");
             return 0;
         } else
             S1 = Ploshad(r1);
         e2 = Perimetr(r1);
         if (e2 == 1) {
-            cout << "Неверный радиус " << endl;
+            printf("Неверный радиус \n");
             return 0;
         } else
             P1 = Perimetr(r1);
-        cout << "Координаты окружности: " << endl;
-        cin >> x2 >> y2;
-        if (x2 != (int)x2 || y2 != (int)y2) {
-            cout << "Координаты должны быть целыми! " << endl;
+        printf("Координаты окружности: \n");
+        if(scanf("%d",&x2) != 1) {
+            printf("Координаты должны быть целыми! \n");
             return 0;
         }
+	if(scanf("%d",&y2) != 1) {
+	    printf("Координаты должны быть целыми!\n");
+	    return 0;
+	}
         printf("Радиус: ");
-        cin >> r2;
+        scanf("%f",&r2);
         e3 = Ploshad(r2);
         if (e3 == 1) {
-            cout << "Неверный радиус " << endl;
+            printf("Неверный радиус \n");
             return 0;
         } else
             S2 = Ploshad(r2);
         e4 = Perimetr(r2);
         if (e4 == 1) {
-            cout << "Неверный радиус " << endl;
+            printf("Неверный радиус \n");
             return 0;
         } else
             
             P2 = Perimetr(r2);    
     } else {
-        cout << "Error. Incorrect data entered. " << endl;
+        printf("Error. Incorrect data entered. \n");
         return 0;
     }
-    cout << name << "( " << x1 << " " << y1 << " " << r1 << ")" << endl;
-    cout << "Ploshad " << S1 << endl;
-    cout << "Perimetr " << P1 << endl << endl;
-    cout << name << "( " << x2 << " " << y2 << " " << r2 << ")" << endl;
-    cout << "Ploshad " << S2 << endl;
-    cout << "Perimetr " << P2 << endl;
+    printf("%s (%d, %d, %f)\n",name, x1, y1, r1);
+    printf("Ploshad %0.2f\n",S1);
+    printf("Perimetr %0.2f\n\n",P1);
+    printf("%s (%d, %d, %f)\n",name, x2, y2, r2);
+    printf("Ploshad %0.2f\n",S2);
+    printf("Perimetr %0.2f\n",P2);
 
     int peres = Intersection(x1, y1, x2, y2, r1, r2);
     if (peres == 0) {
-        cout << "Окружности пересекаются " << endl;
+        printf("Окружности пересекаются \n");
     } else
-        cout << "Окружности не пересекаются " << endl;
+        printf("Окружности не пересекаются \n");
     return 0;
 }
