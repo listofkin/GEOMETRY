75cae87223a9736c1aec34c66f55f50383b437cd
listofkin <listofkin@gmail.com>
2020-05-28 05:03:40 +0700

    Fixed main.c

--- src/main.c
+++ src/main.c
@@ -14,16 +14,16 @@ int main()
     scanf("%s", name);
     if (figure(name) == 0) {
         printf("Координаты окружности: \n");
-        if(scanf("%d",&x1) != 1) {
+        if (scanf("%d", &x1) != 1) {
             printf("Координаты должны быть целыми! \n");
             return 0;
         }
-	if(scanf("%d",&y1) != 1) {
+	    if (scanf("%d", &y1) != 1) {
 	    printf("Координаты должны быть целыми! \n");
 	    return 0;
 	}
         printf("Радиус: ");
-        scanf("%f",&r1);
+        scanf("%f", &r1);
         e1 = Ploshad(r1);
         if (e1 == 1) {
             printf("Неверный радиус \n");
@@ -37,16 +37,16 @@ int main()
         } else
             P1 = Perimetr(r1);
         printf("Координаты окружности: \n");
-        if(scanf("%d",&x2) != 1) {
+        if (scanf("%d", &x2) != 1) {
             printf("Координаты должны быть целыми! \n");
             return 0;
         }
-	if(scanf("%d",&y2) != 1) {
+	    if (scanf("%d", &y2) != 1) {
 	    printf("Координаты должны быть целыми!\n");
 	    return 0;
 	}
         printf("Радиус: ");
-        scanf("%f",&r2);
+        scanf("%f", &r2);
         e3 = Ploshad(r2);
         if (e3 == 1) {
             printf("Неверный радиус \n");
@@ -64,12 +64,12 @@ int main()
         printf("Error. Incorrect data entered. \n");
         return 0;
     }
-    printf("%s (%d, %d, %f)\n",name, x1, y1, r1);
-    printf("Ploshad %0.2f\n",S1);
-    printf("Perimetr %0.2f\n\n",P1);
-    printf("%s (%d, %d, %f)\n",name, x2, y2, r2);
-    printf("Ploshad %0.2f\n",S2);
-    printf("Perimetr %0.2f\n",P2);
+    printf("%s (%d, %d, %f)\n", name, x1, y1, r1);
+    printf("Ploshad %0.2f\n", S1);
+    printf("Perimetr %0.2f\n\n", P1);
+    printf("%s (%d, %d, %f)\n", name, x2, y2, r2);
+    printf("Ploshad %0.2f\n", S2);
+    printf("Perimetr %0.2f\n", P2);
 
     int peres = Intersection(x1, y1, x2, y2, r1, r2);
     if (peres == 0) {
