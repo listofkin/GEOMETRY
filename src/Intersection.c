064a948eadf4903a5454a2020093d271a6958eef
listofkin <listofkin@gmail.com>
2020-05-29 02:43:39 +0700

    Fixed Intersection.c

--- src/Intersection.c
+++ src/Intersection.c
@@ -1,8 +1,7 @@
 #include <math.h>
 #include <stdlib.h>
 
-
-int Intersection(int x1, int y1, int x2, int y2, int r1, int r2) 
+int Intersection(int x1, int y1, int x2, int y2, int r1, int r2)
 {
     float d = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
     if (d == 0 && r1 == r2) {
@@ -10,7 +9,7 @@ int Intersection(int x1, int y1, int x2, int y2, int r1, int r2)
     } else if (d > r1 + r2) {
         return 1;
     } else if (d < abs(r1 - r2)) {
-        return 1;    
-    } else 
+        return 1;
+    } else
         return 0;
 }
